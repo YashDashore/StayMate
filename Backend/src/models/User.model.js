016b7820cb2716788ae781af8a5fc67cf477cb4c
YashDashore <yash.dashore1@gmail.com>
2025-09-20 16:03:46 +0530

    Implement OTP verification and resend functionality; update user model to include verification fields

--- Backend/src/models/User.model.js
+++ Backend/src/models/User.model.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import { Room } from "./Room.model.js";
+import { Tiffin } from "./Tiffin.model.js";
+import { HouseService } from "./HouseService.model.js";
+import { Wifi } from "./WifiSharing.model.js";
+import { Lpg } from "./LpgService.model.js";
+import { RoommateFinder } from "./FindRoommate.model.js";
+import { Booking } from "./Booking.model.js";
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -38,7 +45,14 @@ const UserSchema = new mongoose.Schema({
     profilePhoto: { type: String },
     refreshToken: {
         type: String
-    }
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    otp: { type: String },
+    otpExpiry: { type: Date },
+    lastOtpSentAt : {type : Date}
 }, { timestamps: true });
 
 UserSchema.pre("save", async function (next) {
@@ -46,6 +60,45 @@ UserSchema.pre("save", async function (next) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next()
+});
+
+UserSchema.pre("remove", async function (next) {
+    const userId = this._id;
+    await Room.deleteMany({ owner: userId });
+    await Tiffin.deleteMany({ owner: userId });
+    await HouseService.deleteMany({ provider: userId });
+    await Wifi.deleteMany({ owner: userId });
+    await Lpg.deleteMany({ owner: userId });
+    await Booking.deleteMany({
+        $or: [{ user: userId }, { provider: userId }]
+    })
+
+    const roommatePosts = await RoommateFinder.find({
+        $or: [
+            { createdBy: userId },
+            { currentlyLivingUsers: userId }
+        ]
+    });
+
+    for (const post of roommatePosts) {
+        post.currentlyLivingUsers = post.currentlyLivingUsers.filter(
+            id => id.toString() !== userId.toString()
+        );
+
+        if (post.createdBy.toString() === userId.toString()) {
+            if (post.currentlyLivingUsers.length > 0) {
+                post.createdBy = post.currentlyLivingUsers[0];
+            }
+            else {
+                await RoommateFinder.deleteOne({ _id: post._id });
+                continue;
+            }
+        }
+
+        await post.save();
+    }
+
+    next();
 });
 
 UserSchema.methods.isPasswordCorrect = async function (password) {
--- Backend/src/routes/room.routes.js
+++ Backend/src/routes/room.routes.js
@@ -5,12 +5,21 @@ import {
     updateRoomDetails
 } from "../controllers/room.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { Upload } from "../middleware/multer.middleware.js";
 
 const RoomRouter = Router();
 
-RoomRouter.route("/create").post(verifyJWT, createRoomListing);
+RoomRouter.route("/create").post(verifyJWT,
+    Upload.fields([
+        { name: "photos", maxCount: 7 }])
+    , createRoomListing);
+
+RoomRouter.route("/update/:id").patch(verifyJWT,
+    Upload.fields([
+        { name: "photos", maxCount: 7 },
+    ]), updateRoomDetails)
+
 RoomRouter.route("/delete/:id").delete(verifyJWT, deleteRoomListing)
-RoomRouter.route("/update/:id").patch(verifyJWT, updateRoomDetails)
 RoomRouter.route("/allRooms").get(getAllRooms)
 RoomRouter.route("/myRooms").get(verifyJWT, roomsOwnedByOwner)
 
