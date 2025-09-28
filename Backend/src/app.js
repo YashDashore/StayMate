e43380202ae8a7770b4f50761ddf6b87ed1b1758
YashDashore <yash.dashore1@gmail.com>
2025-09-28 23:13:47 +0530

    Add routing for Tiffin, House Service, and LPG services

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,10 +22,16 @@ app.use(cookieParser())
 
 import UserRouter from "./routes/user.routes.js";
 import RoomRouter from "./routes/room.routes.js";
+import TiffinRouter from "./routes/tiffin.routes.js";
+import ServiceRouter from "./routes/houseService.routes.js";
+import LpgRouter from "./routes/lpgService.routes.js";
 
-// http://localhost:4000/pgHelper/v1/user
+// http://localhost:4000/pgHelper/v1/---
 app.use("/pgHelper/v1/user", UserRouter)
 app.use("/pgHelper/v1/room", RoomRouter)
+app.use("/pgHelper/v1/tiffin", TiffinRouter)
+app.use("/pgHelper/v1/houseService", ServiceRouter)
+app.use("/pgHelper/v1/lpg", LpgRouter)
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");
